f3a4c8e279bebccdb2c7d17c8b4a00de8ddc7f5a
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-09-16 02:08:54 +0530

    addedgitignore,newAssignment

---
+++ expres-assignment/server.js
@@ -0,0 +1,104 @@
+const express = require("express")
+
+const app = express();
+
+const users = require("./express.json")
+
+app.use(express.json())
+
+let d;
+
+app.get("/", middleman({ api_requested_by: "required" }), (req, res) => {
+
+    d.books = users
+
+    res.send(d)
+
+})
+
+
+app.post("/books", middleman({ api_requested_by: "required" }), (req, res) => {
+
+    users.push(req.body)
+    d.books = users
+    res.send(d)
+
+})
+
+app.get("/books/:id", middleman({ api_requested_by: "required" }), (req, res) => {
+
+    let id = req.params.id;
+
+    users.forEach((ele) => {
+        if (ele.id == id) {
+            d.book = ele
+            res.send(d)
+        }
+    })
+
+})
+
+
+app.patch("/books/:id", middleman({ api_requested_by: "required" }), (req, res) => {
+
+    let id = req.params.id;
+
+    users.forEach((ele) => {
+        if (ele.id == id) {
+            console.log(ele, req.body)
+
+            Object.keys(req.body).forEach((e) => {
+                ele[e] = req.body[e]
+            })
+
+            d.book = ele
+
+            res.send(d)
+        }
+    })
+
+})
+
+
+app.delete("/books/:id", middleman({ api_requested_by: "required" }), (req, res) => {
+
+    let id = req.params.id;
+
+    for (let i = 0; i < users.length; i++) {
+
+        if (users[i].id == id) {
+
+            users.splice(i, 1);
+
+            d.book = users
+
+            res.send(d)
+        }
+
+    }
+
+})
+
+
+function middleman(data) {
+    return function (req, res, next) {
+        let errors = [];
+        Object.keys(data).forEach(function (item) {
+            if (!req.query[item]) {
+                errors.push(`Please add ${item} in the form`)
+            }
+        })
+        if (errors.length) {
+            return res.send(errors);
+        }
+
+        d = { "api_requested_by": req.query.api_requested_by }
+        next()
+    }
+}
+
+
+app.listen(3000, function () {
+    console.log("listening on port 3000")
+})
+
--- express-intro/server.js
+++ express-intro/server.js
@@ -2,9 +2,12 @@ console.log("Pratik")
 
 const express = require("express")
 
+//mockoroo.com
 
 const users = require("./express.json")
 const app = express();
+
+app.use(express.json())
 
 app.get("/",function(req,res){
     res.send("Hello")
@@ -15,6 +18,56 @@ app.get("/users",function(req,res){
     res.send(users)
 })
 
+app.post("/users", validate({lastName:"required",Name:"required",gender:"required"}),function(req,res){
+    console.log("Hello")
+    console.log(req.body)
+    res.send(req.body)
+})
+
+// function validate(data){
+//     return function logger1(req,res,next){
+//         console.log("inside logger1")
+
+//         Object.keys(data).forEach(function(item){
+
+//             if(data[item]==="required"){
+//                 if(!req.query[item]){
+//                     res.send(`Need ${item}`)
+//                 }
+    
+//             }
+//         })
+
+//         next()
+//     }
+
+// }
+
+
+function validate(data) {
+    return function(req, res, next) {
+
+        console.log("inside logger1")
+        
+        let errors = [];
+        Object.keys(data).forEach(function (item) {
+            if(data[item] === "required") {
+                if(! req.query[item]) { // req.query.firstName
+                    errors.push(`Please add ${item} in the form`)
+                }
+            }
+        })
+        if(errors.length) {
+            return res.send(errors);
+        }
+        next()
+    }
+}
+
+
 app.listen(2345, function (){
     console.log("listening to 2345")
 })
+
+
+
